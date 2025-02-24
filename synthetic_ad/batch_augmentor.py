90ff69b45d07acc48bfdc413cdc3ae8e74528a65
deyantara1303 <117450381+deyantara1303@users.noreply.github.com>
2025-02-24 12:38:42 +0100

    Add files via upload

--- synthetic_ad/batch_augmentor.py
+++ synthetic_ad/batch_augmentor.py
@@ -11,53 +11,49 @@ class BatchAugmentor:
 		self.num_images = num_images
 
 	def rotate_overlay(self, overlay, angle):
+		# grab the dimensions of the image and calculate the center of the image
 		(h, w) = overlay.shape[:2]
 		center = (w//2, h//2)
+		# rotate image by 'angle' degrees around the center of the image
 		M = cv2.getRotationMatrix2D(center, angle, 1.0)
-		rotated = cv2.warpAffine(overlay, M, (w, h),
-								 borderMode=cv2.BORDER_CONSTANT,
-								 borderValue=(0, 0, 0, 0))
+		rotated = cv2.warpAffine(overlay, M, (w, h))
 		return rotated
 
 	def augment(self):
+		# List all the images in normal and overlay directories
 		normal_images = [os.path.join(self.normal_dir, f) for f in os.listdir(self.normal_dir)
 						 if f.lower().endswith(('.jpg', '.png'))]
 		overlay_images = [os.path.join(self.overlay_dir, f) for f in os.listdir(self.overlay_dir)
 						if f.lower().endswith(('.png'))]
 
 		for i in range(self.num_images):
+			# Randomly select one normal image and one overlay image
 			normal_path = random.choice(normal_images)
 			overlay_path = random.choice(overlay_images)
-
 			normal_img = cv2.imread(normal_path, cv2.IMREAD_COLOR)
 			overlay_img = cv2.imread(overlay_path, cv2.IMREAD_UNCHANGED)
-
 			if overlay_img is None or overlay_img.shape[-1] != 4:
 				print(f"Skipping {overlay_img}: Invalid overlay image")
 				continue
 
-			scale = random.uniform(0.1, 0.3)
-			new_w = int(normal_img.shape[1] * scale)
-			new_h = int(overlay_img.shape[0] * (new_w / overlay_img.shape[1]))
-			overlay_img = cv2.resize(overlay_img, (new_w, new_h), interpolation=cv2.INTER_LINEAR)
-
+			# Rotate overlay image randomly within 360 degrees and save temporarily
 			angle = random.uniform(0, 360)
 			overlay_img = self.rotate_overlay(overlay_img, angle)
-
-			x_pos = random.uniform(0.0, 0.4)
-			y_pos = random.uniform(0.0, 0.3)
-
 			temp_path = os.path.join(self.output_dir, f"overlay{i}.png")
 			cv2.imwrite(temp_path, overlay_img)
 
-			out_path = os.path.join(self.output_dir, f"image_aug_{158:05d}.jpg")
+			# Select random positions for overlay placement and augment over original image
+			x_pos = random.uniform(0.0, 0.4)
+			y_pos = random.uniform(0.0, 0.2)
+			out_path = os.path.join(self.output_dir, f"image_aug_{i:05d}.jpg")
 			iaug.overlay_image(normal_path, temp_path, out_path, x_pos=x_pos, y_pos=y_pos)
 			print(f"Saved augmented image: {out_path}")
 
+			# Remove temporary path
 			os.remove(temp_path)
 
 if __name__=="__main__":
-	# Define directories
+	# Define directories for normal, overlays and results
 	normal_image_dir = "../images/normal/"
 	overlay_image_dir= "../overlays/"
 	augmented_image_dir = "../results"
